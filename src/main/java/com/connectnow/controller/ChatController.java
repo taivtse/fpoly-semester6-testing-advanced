159c2a5d00b96554b3043ba3b58553075564b2b9
Võ Thành Tài <taivtse@gmail.com>
2019-03-28 18:37:36 +0700

    Add hibernate config

--- src/main/java/com/connectnow/controller/ChatController.java
+++ src/main/java/com/connectnow/controller/ChatController.java
@@ -1,12 +1,12 @@
 package com.connectnow.controller;
 
+import com.connectnow.dto.Greeting;
+import com.connectnow.dto.HelloMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.util.HtmlUtils;
-import com.connectnow.dto.Greeting;
-import com.connectnow.dto.HelloMessage;
 
 @Controller
 public class ChatController {
--- src/main/java/com/connectnow/entity/ChatBoxEntity.java
+++ src/main/java/com/connectnow/entity/ChatBoxEntity.java
@@ -1,4 +1,9 @@
 package com.connectnow.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "chat_box")
 public class ChatBoxEntity {
 }
