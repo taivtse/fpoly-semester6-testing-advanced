5dafd0fd63e84321b641a9ea4cbf26df6533bc34
Võ Thành Tài <taivtse@gmail.com>
2019-03-29 07:55:28 +0700

    Update Entity mapping

--- src/main/java/com/connectnow/entity/ChatBoxEntity.java
+++ src/main/java/com/connectnow/entity/ChatBoxEntity.java
@@ -2,6 +2,7 @@ package com.connectnow.entity;
 
 import javax.persistence.*;
 import java.math.BigInteger;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -11,8 +12,15 @@ public class ChatBoxEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private BigInteger id;
 
+    @Column(name = "name")
+    private String name;
+
     @Column(name = "last_message_id", nullable = false)
     private BigInteger lastMessageId;
+
+    @Column(name = "last_message_date", nullable = false)
+    @Temporal(value = TemporalType.DATE)
+    private Date lastMessageDate;
 
     @OneToMany(mappedBy = "chatBoxEntity", fetch = FetchType.EAGER)
     private List<MemberEntity> memberEntityList;
@@ -25,12 +33,28 @@ public class ChatBoxEntity {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public BigInteger getLastMessageId() {
         return lastMessageId;
     }
 
     public void setLastMessageId(BigInteger lastMessageId) {
         this.lastMessageId = lastMessageId;
+    }
+
+    public Date getLastMessageDate() {
+        return lastMessageDate;
+    }
+
+    public void setLastMessageDate(Date lastMessageDate) {
+        this.lastMessageDate = lastMessageDate;
     }
 
     public List<MemberEntity> getMemberEntityList() {
